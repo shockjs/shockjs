bea6c49e2a5b633c9936a4022b5260a1af55acb0
Damian Dennis <damiandennis@gmail.com>
2015-12-16 03:21:08 +1000

    converted to es6. made sure links work as expected.

--- src/shared/js/containers/App.js
+++ src/shared/js/containers/App.js
@@ -1,24 +1,18 @@
-const React = require('react');
-const ReactRouterBootstrap = require('react-router-bootstrap');
-const LinkContainer = ReactRouterBootstrap.LinkContainer;
-const ReactBootstrap = require('react-bootstrap');
-const Nav = ReactBootstrap.Nav;
-const Navbar = ReactBootstrap.Navbar;
-const NavBrand = ReactBootstrap.NavBrand;
-const NavItem = ReactBootstrap.NavItem;
-const ReactRouter = require('react-router');
-const Link = ReactRouter.Link;
+import React from 'react';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import { Nav, Navbar, NavBrand, NavItem } from 'react-bootstrap';
+import { Link } from 'react-router';
 
-module.exports = React.createClass({
+export default React.createClass({
   render() {
     return (
       <div>
         <Navbar>
           <NavBrand><Link to="/">ShockJS</Link></NavBrand>
           <Nav>
-            <LinkContainer to="/"><NavItem eventKey={1}>Welcome</NavItem></LinkContainer>
-            <LinkContainer to="/contact-us"><NavItem eventKey={2}>Contact Us</NavItem></LinkContainer>
-            <LinkContainer to="/login"><NavItem eventKey={3}>Login</NavItem></LinkContainer>
+            <IndexLinkContainer to="/"><NavItem>Welcome</NavItem></IndexLinkContainer>
+            <LinkContainer to="/contact-us"><NavItem>Contact Us</NavItem></LinkContainer>
+            <LinkContainer to="/login"><NavItem>Login</NavItem></LinkContainer>
           </Nav>
         </Navbar>
 
